d505ea61bc485762497750118d74e6d1796e2f8c
Buchi-K <jack4711jack@icloud.com>
2022-03-14 22:35:16 +0900

    first commit

---
+++ home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>○○掲示板</title>
+</head>
+<body>
+  <div id="container">
+    <div class="title">
+      <h1>ようこそ〇〇掲示板へ</h1>
+    </div>
+    
+
+
+    会員登録画面
+
+
+    ゲストでやる
+
+
+
+
+  </div>
+
+</body>
+</html>
