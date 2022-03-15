fc5b3bcb4622c29ca171c00e9880a40f02ab5961
Buchi-K <jack4711jack@icloud.com>
2022-03-15 23:29:42 +0900

    編集

--- boards.php
+++ boards.php
@@ -4,15 +4,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
-  <title>会員登録</title>
+  <title>掲示板一覧</title>
 </head>
 <body>
-  <div id="box">
-    <div class="resister">
-      <h1>会員登録画面</h1>
-    </div>
-  </div>
-
+  
 </body>
 </html>
